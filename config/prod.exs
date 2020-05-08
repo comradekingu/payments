536a2184a9bc8df9f43f685659b7062af581d820
Blake Kostner <git@btkostner.io>
2020-05-07 18:46:43 -0600

    ci: add production build docker file

--- config/prod.exs
+++ config/prod.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 # For production, don't forget to configure the url host
 # to something meaningful, Phoenix uses this information
@@ -11,7 +11,12 @@ use Mix.Config
 # before starting your production server.
 config :payments, PaymentsWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  http: [
+    port: 80,
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -49,7 +54,3 @@ config :logger, level: :info
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
-
-# Finally import the config/prod.secret.exs which loads secrets
-# and configuration from environment variables.
-import_config "prod.secret.exs"
